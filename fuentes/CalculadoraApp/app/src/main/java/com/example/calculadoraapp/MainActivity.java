6db453b76e487cb1f47451eea36cd922d54ee1f0
Christian Angulo <christiananguloprogramador23@gmail.com>
2020-08-27 12:11:27 -0400

    Avanze TextView colors xml string xml

--- fuentes/CalculadoraApp/app/src/main/java/com/example/calculadoraapp/MainActivity.java
+++ fuentes/CalculadoraApp/app/src/main/java/com/example/calculadoraapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.calculadoraapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void saludar(View v){
+        Toast.makeText(context this, text:"hola",Toast.LENGTH_LONG).show();
+        )
     }
 }
